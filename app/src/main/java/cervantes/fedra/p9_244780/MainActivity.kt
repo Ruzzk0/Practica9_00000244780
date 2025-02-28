97fd72a21f7e033b32cef7bf5d8fab9112ffe256
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2025-02-27 21:57:44 -0700

    correcion
    errores de sintaxys en la parte de main

--- app/src/main/java/cervantes/fedra/p9_244780/MainActivity.kt
+++ app/src/main/java/cervantes/fedra/p9_244780/MainActivity.kt
@@ -1,20 +1,58 @@
 package cervantes.fedra.p9_244780
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private val userRef = FirebaseDatabase.getInstance().getReference("Users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val btnSave: Button = findViewById(R.id.save_button)
+        btnSave.setOnClickListener { saveMarkFromForm() }
+
+        userRef.addChildEventListener(object : ChildEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {}
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousName: String?) {}
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousName: String?) {}
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+
+            override fun onChildAdded(dataSnapshot: DataSnapshot, p1: String?) {
+                val usuario = dataSnapshot.getValue(User::class.java)
+                if (usuario != null) writeMark(usuario)
+            }
+        })
+    }
+
+    private fun saveMarkFromForm() {
+        val name: EditText = findViewById(R.id.et_name)
+        val lastName: EditText = findViewById(R.id.et_lastName)
+        val age: EditText = findViewById(R.id.et_age)
+
+        val usuario = User(
+            name.text.toString(),
+            lastName.text.toString(),
+            age.text.toString()
+        )
+        userRef.push().setValue(usuario)
+    }
+
+    private fun writeMark(mark: User) {
+        val listV: TextView = findViewById(R.id.list_textView)
+        val text = listV.text.toString() + mark.toString() + "\n"
+        listV.text = text
     }
 }
--- app/src/main/java/cervantes/fedra/p9_244780/User.kt
+++ app/src/main/java/cervantes/fedra/p9_244780/User.kt
@@ -1,7 +1,9 @@
 package cervantes.fedra.p9_244780
 
-data class User(var firstName: String?=null,
-                var lastName: String?=null,
-                var aqe: String?=null){
-    override fun toString()=firstName+"\t"+lastName+"\t"+age
+data class User(
+    var firstName: String? = null,
+    var lastName: String? = null,
+    var age: String? = null
+) {
+    override fun toString() = "$firstName\t$lastName\t$age"
 }
