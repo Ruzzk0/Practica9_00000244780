182a6556dabadde0a68d226914692c54cf860033
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2025-02-27 21:47:44 -0700

    inicios de conexion de base
    Creacion tambien del USER

---
+++ app/src/main/java/cervantes/fedra/p9_244780/User.kt
@@ -0,0 +1,7 @@
+package cervantes.fedra.p9_244780
+
+data class User(var firstName: String?=null,
+                var lastName: String?=null,
+                var aqe: String?=null){
+    override fun toString()=firstName+"\t"+lastName+"\t"+age
+}
